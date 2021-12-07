21c8f124ac2119559f1ad34846899c5ffe9f2389
Stephen McMurtry <smcmurtry@gmail.com>
2021-12-06 22:35:17 -0500

    added an index page and display meetings by type

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -3,21 +3,37 @@ from dotenv import load_dotenv
 from flask import Flask, redirect, render_template, url_for
 import json
 from scraping.scraping import Meeting
+from typing import List, Dict
 
 load_dotenv()
 
-meeting_data: Meeting = []
+meeting_data: List[Meeting] = []
 with open("scraping/all_data_flat.json", "r") as f:
     meeting_data = json.load(f)
+
+all_meeting_types: List[str] = []
+with open("scraping/data/all_meeting_types.json", "r") as f:
+    all_meeting_types = json.load(f)
+
+meeting_data_dict: Dict[str, List] = {}
+for meeting_type in all_meeting_types:
+    meeting_data_dict[meeting_type] = [x for x in meeting_data if x["meeting_type"] == meeting_type]
+
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
 
-    # a simple page that says hello
     @app.route('/')
-    def hello():
-        variables = {"meeting_data": meeting_data[:30]}
+    def home():
+        variables = {"all_meeting_types": all_meeting_types}
+        return render_template('home.html', **variables)
+
+    @app.route('/meetings/<meeting_type>', methods = ['GET'])
+    def meetings(meeting_type):
+        if meeting_type not in meeting_data_dict:
+            return render_template("404.html")
+        variables = {"meeting_data": meeting_data_dict[meeting_type]}
         return render_template('meeting_list.html', **variables)
 
 
