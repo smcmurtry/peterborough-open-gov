44ce00fbeae651e83881651522be5322b45afa1b
Stephen McMurtry <smcmurtry@gmail.com>
2022-10-18 01:07:49 +0000

    playwright fixes

--- playwright/tests/city-council.spec.ts
+++ playwright/tests/city-council.spec.ts
@@ -12,23 +12,20 @@ var logger = fs.createWriteStream('output/output.txt', {
 })
 
 test('test', async ({ page }) => {
-    // await page.goto('https://pub-peterborough.escribemeetings.com/meetingscalendarview.aspx?FillWidth=1&wmode=transparent');
-    await page.goto('https://pub-peterborough.escribemeetings.com/meetingscalendarview.aspx?FillWidth=1&wmode=transparent&Year=2022');
+    await page.goto('https://pub-peterborough.escribemeetings.com/meetingscalendarview.aspx?FillWidth=1&wmode=transparent');
+    // await page.goto('https://pub-peterborough.escribemeetings.com/meetingscalendarview.aspx?FillWidth=1&wmode=transparent&Year=2022');
     delay(5000)
     var pastMeetingLocator = page.locator(".past-meetings")
     var accordians = pastMeetingLocator.getByRole('button', { expanded: false })
     var accordianCount = await accordians.count();
+    console.log("accordianCount", accordianCount)
     for (let i = 0; i < accordianCount; i++) {
-        var _locator = accordians.nth(i);
-        await _locator.click();
-        await delay(10000)
-        var allCalendarItems = page.locator(".calendar-item")
-        var calendarItemsCount = await allCalendarItems.count()
-        expect(calendarItemsCount).toBeGreaterThan(0)
-        for (let i = 0; i < calendarItemsCount; i++) {
-            var innerHtml = await allCalendarItems.nth(i).innerHTML()
-            logger.write(innerHtml);
-        }
+        // for (let i = 0; i < 3; i++) {
+        console.log("i=", i)
+        accordians.nth(i).click();
+        await delay(20000)
     }
 
+    var pastMeetingHtml = await pastMeetingLocator.innerHTML()
+    logger.write(pastMeetingHtml);
 });
