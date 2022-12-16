8638b3d4f2e716b19d532a25b18ab6944ffa0846
Stephen McMurtry <smcmurtry@gmail.com>
2022-12-16 01:14:06 +0000

    ran the main_runner

--- scraping/main_runner.sh
+++ scraping/main_runner.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
+echo "Parsing scraped data"
 python parse_scraped_data.py \
-    --input_fpath ../playwright_output \
+    --input_dir ../playwright_output \
     --output_fpath generated_data/all_data_flat.json
 
+echo "Saving agendas"
 python save_agendas.py \
     --flat_data_fpath generated_data/all_data_flat.json \
     --agenda_output_dir agenda
 
+echo "Downloading minutes"
 python download_minutes.py \
     --agenda_input_dir agenda \
     --minutes_output_dir minutes
 
+echo "Generating all_meeting_data.json"
 python generate_all_meeting_data.py \
     --flat_data_fpath_input generated_data/all_data_flat.json \
     --minutes_dir_input minutes \
