febf27d1fc97918da13c301db962676d66072134
Stephen McMurtry <stephenmcmurtry@Stephens-MacBook-Air.local>
2022-12-18 19:29:23 -0500

    fixed typo

--- scraping/sync_to_s3.sh
+++ scraping/sync_to_s3.sh
@@ -4,5 +4,5 @@
 # aws s3 cp ../playwright_output/output.2022.txt s3://${CITY_COUNCIL_SCRAPER_S3_BUCKET}/playwright_output/
 # aws s3 cp ../playwright_output/output.historical.txt s3://${CITY_COUNCIL_SCRAPER_S3_BUCKET}/playwright_output/
 
-aws s3 sync minutes s3:/${CITY_COUNCIL_SCRAPER_S3_BUCKET}/minutes
+aws s3 sync minutes s3://${CITY_COUNCIL_SCRAPER_S3_BUCKET}/minutes
 aws s3 cp generated_data/all_meeting_data.json s3://${CITY_COUNCIL_SCRAPER_S3_BUCKET}/
