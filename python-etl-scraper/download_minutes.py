7c7bc8a92a9a3a36b943caf7d614f16dd5d9f166
Stephen McMurtry <smcmurtry@gmail.com>
2025-02-25 01:24:28 +0000

    removed unused data, fix dependencies

--- python-etl-scraper/download_minutes.py
+++ python-etl-scraper/download_minutes.py
@@ -6,68 +6,74 @@ import requests
 import time
 
 
-def main(agenda_input_dir="python-etl-scraper/agenda", minutes_output_dir="python-etl-scraper/minutes"):
-    agenda_fnames = os.listdir(agenda_input_dir)
+def download_minutes_from_agenda(agenda_input_dir, agenda_fname, minutes_output_dir):
+    """
+    This function takes an html agenda file and downloads the minutes pdfs.
+    The minutes pdfs are for past meetings - they are approved in the current meeting.
+    """
+    meeting_type_str = agenda_fname.split(".")[1]
+    f = open(f"{agenda_input_dir}/{agenda_fname}", "r")
+    agenda_text = f.read()
+    f.close()
+    soup = BeautifulSoup(agenda_text, "html.parser")
+    attachment_divs = soup.find_all("div", attrs={"class": "AgendaItemAttachment"})
+    for attachment in attachment_divs:
+        a = attachment.find("a")
+        if not a:
+            continue
+        href = a.attrs["href"]
+        url = f"https://pub-peterborough.escribemeetings.com/{href}"
 
-    for n, agenda_fname in enumerate(agenda_fnames):
-        print(n, end=", ")
-        meeting_type_str = agenda_fname.split(".")[1]
-        f = open(f"{agenda_input_dir}/{agenda_fname}", "r")
-        agenda_text = f.read()
-        f.close()
-        soup = BeautifulSoup(agenda_text, "html.parser")
-        attachment_divs = soup.find_all("div", attrs={"class": "AgendaItemAttachment"})
-        for attachment in attachment_divs:
-            a = attachment.find("a")
-            if not a:
+        if "minutes" in a.text.lower():
+            try:
+                possible_date = a.text.split(" - ")[1].split(" ")[-1]
+                date = datetime.strptime(possible_date, "%m-%d-%Y")
+            except:
                 continue
-            href = a.attrs["href"]
-            url = f"https://pub-peterborough.escribemeetings.com/{href}"
+        else:
+            possible_date = a.text[:10]
+            if possible_date.count("-") != 2:
+                continue
 
-            if "minutes" in a.text.lower():
+            if " " in possible_date:
+                possible_short_date = possible_date.split(" ")[0]
                 try:
-                    possible_date = a.text.split(" - ")[1].split(" ")[-1]
-                    date = datetime.strptime(possible_date, "%m-%d-%Y")
+                    date = datetime.strptime(possible_short_date, "%m-%d-%y")
                 except:
                     continue
             else:
-                possible_date = a.text[:10]
-                if possible_date.count("-") != 2:
-                    continue
-
-                if " " in possible_date:
-                    possible_short_date = possible_date.split(" ")[0]
+                try:
+                    date = datetime.strptime(possible_date, "%m-%d-%Y")
+                except ValueError:
                     try:
-                        date = datetime.strptime(possible_short_date, "%m-%d-%y")
+                        date = datetime.strptime(possible_date, "%Y-%m-%d")
                     except:
                         continue
-                else:
-                    try:
-                        date = datetime.strptime(possible_date, "%m-%d-%Y")
-                    except ValueError:
-                        try:
-                            date = datetime.strptime(possible_date, "%Y-%m-%d")
-                        except:
-                            continue
 
-            date_str = datetime.strftime(date, "%Y-%m-%d")
-                
-            new_fname = f"{date_str}.{meeting_type_str}.pdf"
-            new_fpath = f"{minutes_output_dir}/{new_fname}"
+        date_str = datetime.strftime(date, "%Y-%m-%d")
+            
+        new_fname = f"{date_str}.{meeting_type_str}.pdf"
+        new_fpath = f"{minutes_output_dir}/{new_fname}"
 
-            # do not overwrite
-            if os.path.isfile(new_fpath):
-                continue
+        # do not overwrite
+        if os.path.isfile(new_fpath):
+            continue
 
-            # download
-            resp = requests.get(url)
+        # download
+        resp = requests.get(url)
 
-            with open(new_fpath, "wb") as f:
-                f.write(resp.content)
-            print(f"download and save {new_fpath}")
+        with open(new_fpath, "wb") as f:
+            f.write(resp.content)
+        print(f"download and save {new_fpath}")
 
-            time.sleep(2)
+        time.sleep(2)
 
+
+def main(agenda_input_dir="python-etl-scraper/agenda", minutes_output_dir="python-etl-scraper/minutes"):
+    agenda_fnames = os.listdir(agenda_input_dir)
+    for n, agenda_fname in enumerate(agenda_fnames):
+        print(n, end=", ")
+        download_minutes_from_agenda(agenda_input_dir, agenda_fname, minutes_output_dir)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
